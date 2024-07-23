834fd11472307f64906260fe31388e3b4e6e36cb
biswajit-github-2022 <ranabiswajit911@gmail.com>
2024-07-23 21:50:23 +0530

    update

---
+++ class/RDBMS/PHP/05_variable.php
@@ -0,0 +1,10 @@
+<?php
+
+echo 'this is s5';
+
+$name="Biswajit";
+$income=50000;
+echo $name;
+echo $income;
+
+?>
---
+++ class/RDBMS/PHP/sample.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>This is Title</title>
+</head>
+<body>
+    <h2>Hello World</h2>
+    <?php
+    include "05_variable.php";
+    echo "<P> hELLO <P> <br>";
+    echo '<a href="http://www.google.com">Google</a>';
+    echo 5+8;
+
+    
+    echo 'hello'
+    ?>
+    <h2>End</h2>
+</body>
+</html>
