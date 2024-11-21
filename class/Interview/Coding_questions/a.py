572b5da7ede66d75b5bfad2f8646af08d93acd83
biswajit-github-2022 <biswajitranaplanet@gmail.com>
2024-11-21 18:51:48 +0530

    update

---
+++ class/Interview/Coding_questions/a.py
@@ -0,0 +1,8 @@
+class car:
+    def __init__ (self,model,year):
+        self.model=model
+        self.year=year
+    def showDetail(self):
+        print(f"this car is ${self.model} of year ${self.year}")
+        
+        
---
+++ class/Interview/Coding_questions/b.py
@@ -0,0 +1,4 @@
+from a import car
+
+new_car=car("x","y")
+print(new_car.showDetail())
